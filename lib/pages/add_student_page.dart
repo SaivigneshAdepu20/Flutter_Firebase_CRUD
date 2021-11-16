b40f1ed1ef62e107b0421d66c8db92b532f7131e
SaivigneshAdepu20 <saivigneshadepu@gmail.com>
2021-11-16 12:24:04 +0530

    crud operations done

---
+++ lib/pages/add_student_page.dart
@@ -0,0 +1,177 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AddStudentPage extends StatefulWidget {
+  AddStudentPage({Key? key}) : super(key: key);
+
+  @override
+  _AddStudentPageState createState() => _AddStudentPageState();
+}
+
+class _AddStudentPageState extends State<AddStudentPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  var name = "";
+  var email = "";
+  var password = "";
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  clearText() {
+    nameController.clear();
+    emailController.clear();
+    passwordController.clear();
+  }
+
+  // Adding Student
+  CollectionReference students =
+      FirebaseFirestore.instance.collection('students');
+
+  Future<void> addUser() {
+    return students
+        .add({'name': name, 'email': email, 'password': password})
+        .then((value) => print('User Added'))
+        .catchError((error) => print('Failed to Add user: $error'));
+    // print("User added");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add New Student"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    labelText: 'Name: ',
+                    labelStyle: const TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0)),
+                    errorStyle:
+                        const TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    labelText: 'Email: ',
+                    labelStyle: const TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0)),
+                    errorStyle:
+                        const TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password: ',
+                    labelStyle: const TextStyle(fontSize: 20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0)),
+                    errorStyle:
+                        const TextStyle(color: Colors.redAccent, fontSize: 15),
+                  ),
+                  controller: passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, otherwise false.
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            name = nameController.text;
+                            email = emailController.text;
+                            password = passwordController.text;
+                            addUser();
+                            clearText();
+                          });
+                        }
+                      },
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      )),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {clearText()},
+                      child: const Text(
+                        'Reset',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                          )),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'list_student_page.dart';
+import 'add_student_page.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Flutter Firebase CRUD'),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddStudentPage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Add',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                )),
+          ],
+        ),
+      ),
+      body: ListStudentPage(),
+    );
+  }
+}
